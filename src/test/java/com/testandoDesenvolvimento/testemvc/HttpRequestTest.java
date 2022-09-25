8ca4b808ddd453a2aedde6f34d92bbe4c1556933
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-09-25 16:33:54 -0300

    projeto web mvc teste, primeiro versão do codigo para pratica de aula

---
+++ src/test/java/com/testandoDesenvolvimento/testemvc/HttpRequestTest.java
@@ -0,0 +1,34 @@
+package com.testandoDesenvolvimento.testemvc;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;//estava faltando
+import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.web.server.LocalServerPort; // anotação correta mais esta desativada, substituida pela linha abaixo
+import org.springframework.boot.test.web.server.LocalServerPort;//anotação incorreta
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// verificando a sanidade, testar o comportamento da aplicação
+// indica-se aqui a conexão enviando uma solicitaçao HTTP e verificando a resposta
+
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // estava errado
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    //@LocalServerPort // estava ligada a importação import org.springframework.boot.test.web.server.LocalServerPort;
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception{
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
+}
